dc9b601443c14f9770976bf039ff9f9c0d49147e
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-02-14 09:47:46 +0200

    Add CORS middleware to FastAPI application for cross-origin requests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.models import ALL_DB_MODELS
 from src.routers import ALL_ROUTERS
@@ -13,6 +14,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="Pet The Plant API", lifespan=lifespan)
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 for router in ALL_ROUTERS:
     app.include_router(router)
