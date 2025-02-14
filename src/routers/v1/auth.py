c6db4452aebafae4ed1379de8bc6b1a25a22188a
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-02-14 23:52:42 +0200

    Enhance security of refresh token handling by adding SameSite and Secure attributes; improve error handling for missing refresh token

--- src/routers/v1/auth.py
+++ src/routers/v1/auth.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import APIRouter, Cookie, HTTPException, Response
 from passlib.hash import bcrypt
@@ -12,14 +12,19 @@ router = APIRouter(tags=["auth"], prefix="/v1/auth")
 @router.post("/signup")
 async def signup(body: UserCreate, response: Response):
     """Sign up a new user"""
-    user = await user_service.get_by_email(body.email) 
+    user = await user_service.get_by_email(body.email)
     if user:
         raise HTTPException(status_code=400, detail="User already exists")
     
     user = await user_service.create(body.email, body.name, bcrypt.hash(body.password))
     token = await auth_service.generate(user.id)
-    response.set_cookie("refresh_token", token.id, httponly=True)
-
+    response.set_cookie(
+        key="refresh_token",
+        value=token.id,
+        httponly=True,
+        samesite="None",
+        secure=True
+    )
     return {"token": user.generate_token()}
 
 
@@ -34,14 +39,22 @@ async def login(body: UserAuth, response: Response):
         raise HTTPException(status_code=400, detail="Invalid password")
     
     token = await auth_service.generate(user.id)
-    response.set_cookie("refresh_token", token.id, httponly=True)
-    
+    response.set_cookie(
+        key="refresh_token",
+        value=token.id,
+        httponly=True,
+        samesite="None",
+        secure=True
+    )
     return {"token": user.generate_token()}
 
 
 @router.post("/exchange")
-async def exchange(refresh_token: Annotated[str, Cookie] = Cookie()):
+async def exchange(refresh_token: Optional[str] = Cookie(None)):
     """Exchange a refresh token for a new access token"""
+    if not refresh_token:
+        raise HTTPException(status_code=400, detail="Missing refresh token")
+    
     old_token = await auth_service.get_by_token(refresh_token)
     if not old_token:
         raise HTTPException(status_code=400, detail="Invalid token")
