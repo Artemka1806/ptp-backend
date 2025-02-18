96d1d87caec808567d03667c01ec26e0ba539857
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-02-18 18:23:52 +0200

    Add WebSocket endpoint for retrieving user plants and enhance current user authentication

--- src/routers/v1/user.py
+++ src/routers/v1/user.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
-from passlib.hash import bcrypt
+from asyncio import sleep as async_sleep
+
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect, WebSocketException
+from fastapi.encoders import jsonable_encoder
 
 from src.models import User
 from src.services import user_service
@@ -18,3 +20,24 @@ async def me(user: User = Depends(current_user)):
 async def get_user_plants(user: User = Depends(current_user)):
     """Get all plants owned by the current user"""
     return await user_service.get_plants(user)
+
+
+@router.websocket("/plants")
+async def get_user_plants_ws(websocket: WebSocket):
+    token = websocket.query_params.get("token")
+    try:
+        user = await current_user(token=token)
+    except Exception as e:
+        await websocket.close(code=1008)
+        return
+    await websocket.accept()
+    try:
+        while True:
+            data = await user_service.get_plants(user)
+            encoded_data = jsonable_encoder(data)
+            await websocket.send_json(encoded_data)
+            await async_sleep(10)
+    except (WebSocketDisconnect, WebSocketException):
+        pass
+    finally:
+        await websocket.close()
--- src/utils/current_user.py
+++ src/utils/current_user.py
@@ -1,18 +1,21 @@
+from typing import Optional
+
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 security = HTTPBearer()
 
 async def current_user(
-    auth: HTTPAuthorizationCredentials = Security(security)
+    token: Optional[str] = None,
+    auth: HTTPAuthorizationCredentials = Security(security),
 ):
     from src.models import User
-    
-    if not auth:
-        raise HTTPException(401, "No authorization credentials found")
-    
-    token = auth.credentials
-    
+
+    if token is None:
+        if not auth:
+            raise HTTPException(401, "No authorization credentials found")
+        token = auth.credentials
+
     user = await User.get_by_token(token)
     if user is None:
         raise HTTPException(404, "Authorized user could not be found")
